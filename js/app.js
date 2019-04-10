307a333f787f1dd84b1c2af5b092e4da0e5b9054
Zach Mikus <zachmikus@Zachs-MacBook-Pro.local>
2019-04-10 09:45:23 -0500

    Wednesday AM Commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 //- - - - - - - - - - - - - - - - - - - - - CLASSES
-// class Player {
-// 	constructor() {
-// 		name: name 
-// 		hand: [Card]
-// 		deck: []
-// 		score: 0
-// 	}
-// }
 
 class Card {
 	constructor(number, suit, value) {
@@ -33,27 +25,23 @@ class Deck {
 			}
 		}
 		
-
 	shuffle (array) {
 		let i = array.length
 		let j = 0
 		let temp = null
 
-			for (let i = array.length - 1; i > 0; i -= 1) {
-				j = Math.floor(Math.random() * (i +1));
-				temp = array[i];
+			for(let i = array.length -1; i > 0; i -= 1) {
+				j = Math.floor(Math.random() * (i + 1));
+				temp = array [i];
 				array[i] = array[j];
 				array[j] = temp;
-			}	
+			}
 			return array;
-	}
-}
-
-
-
-
+	}	
+};
 
 //- - - - - - - - - - - - - - - - - - - - - GAME
+
 const game = {
 	gameOn: false,
 	player1Card: [],
@@ -70,71 +58,78 @@ const game = {
 			const gameDeck = new Deck
 			gameDeck.shuffle(gameDeck.deck)
 			this.fillGameDeck(gameDeck.deck)
-			//console.log(this.shuffledDeck, ' This is the shuffledDeck');
+			console.log(this.shuffledDeck, ' This is the shuffledDeck');
 			this.dealDeck(this.shuffledDeck)
 		}
 	},
 	fillGameDeck(array) {
-		for(let i = 0; i < array.length; i++) {
+		for(let i = 0; i < this.array.length; i++) {
 			this.shuffledDeck.push(array[i])
 		}
 	},
 	dealDeck (array) {
-		for(let i = 0; i < array.length - 1; i + 2){
+		for(let i = 0; i < this.array.length - 1; i + 2){
 			this.player1Deck.push(array[i])
 			this.player2Deck.push(array[i + 1])
 			array.splice(i, 1)
 			array.splice(i + 1, 1)
-			//console.log(this.player1Deck, ' This is player 1');
-			//console.log(this.player2Deck, ' This is player 2');
+			console.log(this.player1Deck, ' This is player 1');
+			console.log(this.player2Deck, ' This is player 2');
 		}
-	}
+	},
+
 
 //moves card from game.player1Deck to game.player1Card
 	playCard() {
-		this.player1Card.unshift(player1Deck[0])
-		this.player2Card.unshift(player2Deck[0])
-		console.log();
+		this.player1Card = this.player1Deck[0]
+		console.log(player1Card + 'this is player1Card')
+		this.player1Card.unshift(this.player1Deck[0])
+		this.player2Card.unshift(this.player2Deck[0])
+		console.log(this.player1Card);
  	},
- }
+ 
 
-// 	checkHandWinner() {
-// 	 	if(this.player1Card.value > this.player2Card.value){
-// 			this.player1Score = (player1Score += 2)
-// 			this.player1Deck.push(this.player1Card)
-// 			this.player1Deck.push(this.player2Card)
+	checkHandWinner() {
+	 	if(this.player1Card.value > this.player2Card.value){
+			this.player1Score = (this.player1Score += 2)
+			this.player1Deck.push(this.player1Card)
+			this.player1Deck.push(this.player2Card)
 
-// 		}else if(this.player1Card.value < this.player2Card.value){
-// 			this.player2Score = (player2Score += 2)
-// 			this.player2Deck.push(this.player1Card)
-// 			this.player2Deck.push(this.player2Card)
+		}else if(player1Card.value < player2Card.value){
+			this.player2Score = (this.player2Score += 2)
+			this.player2Deck.push(this.player1Card)
+			this.player2Deck.push(this.player2Card)
 
-// 	 	}else{
-// 	 		this.player1Score = (player1Score += 1)
-// 	 		this.player1Deck.push(this.player1Card)
-// 	 		this.player2Score = (player2Score += 1)
-// 	 		this.player2Deck.push(this.player2Card)
-// 	 	}
-// 	},
-// 	// declareWinner(){
-// 	// 	// if 
-// 	// }
-// }
+	 	}else{
+	 		this.player1Score = (this.player1Score += 1)
+	 		this.player1Deck.push(player1Card)
+	 		this.player2Score = (this.player2Score += 1)
+	 		this.player2Deck.push(this.player2Card)
+	 	}
+	},
+declareWinner(){
+		if(this.player1Deck == 0 || this.player2Deck == 0){
+
+		}
+ 
+	},
+
+}
 
 
+//- - - - - - - - - - - - - - - - - - - - - LISTENERS
 
-// //- - - - - - - - - - - - - - - - - - - - - LISTENERS
-// $('#begin-btn').on('click', () => {
-// 	game.startGame()
-// });
+$('#begin-btn').on('click', () => {
+	game.startGame()
+});
 
-// $('.player1Deck').on('click', () => {
+$('.player1Deck').on('click', () => {
 
-// });
+});
 
-// $('.player2').on('click', () => {
+$('.player2Deck').on('click', () => {
 
-// });
+});
 
 
 
